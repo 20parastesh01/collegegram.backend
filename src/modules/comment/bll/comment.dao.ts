d6c0f534ccf03d5d976f930e2040aab305debe1c
erfannouri.en <erfannouri.en@gmail.com>
2023-09-29 06:36:50 +0330

    exclude likes and bookmarks from user in dao and correct commentDao use transform function

--- src/modules/comment/bll/comment.dao.ts
+++ src/modules/comment/bll/comment.dao.ts
@@ -7,14 +7,13 @@ import { CommentId } from '../model/comment-id'
 const convertToModel = (entity: CommentEntity): Comment => {
     const { updatedAt, post, likes, author, id, ...rest } = entity
     const ID = id ?? (0 as CommentId)
-    return { id:ID, author: zodUserShort.parse({ photo: '', ...author }), ...rest }
+    return { id: ID, author: zodUserShort.parse({ photo: '', ...author }), ...rest }
 }
 
 export const commentDao = (input: CommentEntity) => {
     return {
-        toComment(): Comment | undefined {
-            const { updatedAt, author, ...rest } = input
-            return { author: zodUserShort.parse({ photo: '', ...author }), ...rest }
+        toComment(): Comment {
+            return convertToModel(input)
         },
     }
 }
