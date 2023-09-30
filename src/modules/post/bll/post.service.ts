85f7eb236d7bf1ec8b9559f8a1a67ce0ce93597c
20Parastesh01 <parastesh.al2001@gmail.com>
2023-10-01 00:11:06 +0330

    added profile photo to get target user and explore

--- src/modules/post/bll/post.service.ts
+++ src/modules/post/bll/post.service.ts
@@ -68,7 +68,7 @@ export class PostService implements IPostService {
         const relation = (await this.relationService.getRelations(userId, targetUserId)).relation
         if (!this.validateAccess(targetUser, relation)) return { msg: messages.postAccessDenied.persian }
         const closeFriend = await this.checkCloseFriend(userId, targetUserId)
-        const posts = (await this.postRepo.findAllBasicPosts(targetUserId, closeFriend))
+        const posts = await this.postRepo.findAllBasicPosts(targetUserId, closeFriend)
         if (posts.length < 1) return { msg: messages.postNotFound.persian }
 
         const postsWithPhotos = await Promise.all(posts.map((post) => this.adjustPhoto(post)))
@@ -76,7 +76,7 @@ export class PostService implements IPostService {
     }
 
     async getMyPosts(userId: UserId) {
-        const posts = (await this.postRepo.findAllBasicPosts(userId, [true, false]))
+        const posts = await this.postRepo.findAllBasicPosts(userId, [true, false])
         if (posts.length < 1) return { msg: messages.postNotFound.persian }
 
         const postsWithPhotos = await Promise.all(posts.map((post) => this.adjustPhoto(post)))
@@ -91,20 +91,22 @@ export class PostService implements IPostService {
         const posts = await Promise.all(
             users.map(async (targetUser) => {
                 const closeFriend = await this.checkCloseFriend(userId, targetUser.id)
-                const posts = (await this.postRepo.findAllFullPosts(targetUser.id, closeFriend))
+                const posts = await this.postRepo.findAllFullPosts(targetUser.id, closeFriend)
                 return posts
             })
         )
 
         const flattenedPosts = posts.flat()
         flattenedPosts.sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
-        const postsWithLiked = await Promise.all(flattenedPosts.map(async(post)=>{
-            const like = await (services['LikeService'] as LikeService).getLikeByUserAndPost(userId, post.id)
-            const bookmark = await (services['BookmarkService'] as BookmarkService).getBookmarkByUserAndPost(userId, post.id)
-            post.ifBookmarked = bookmark
-            post.ifLiked = like
-            return post
-        }))
+        const postsWithLiked = await Promise.all(
+            flattenedPosts.map(async (post) => {
+                const like = await (services['LikeService'] as LikeService).getLikeByUserAndPost(userId, post.id)
+                const bookmark = await (services['BookmarkService'] as BookmarkService).getBookmarkByUserAndPost(userId, post.id)
+                post.ifBookmarked = bookmark
+                post.ifLiked = like
+                return post
+            })
+        )
 
         if (postsWithLiked.length < 1) return { msg: messages.postNotFound.persian }
         const postsWithPhotos = await Promise.all(postsWithLiked.map((post) => this.adjustPhoto(post)))
@@ -115,7 +117,7 @@ export class PostService implements IPostService {
     async createPost(dto: CreatePostDTO, files: Express.Multer.File[], userId: UserId) {
         if (files.length < 1) return { msg: messages.failed.persian }
         const createPostRepoInput = toCreatePost({ ...dto, author: userId })
-        const createdPost = (await this.postRepo.create(createPostRepoInput))
+        const createdPost = await this.postRepo.create(createPostRepoInput)
         if (!createdPost) return new ServerError(PersianErrors.ServerError)
 
         await MinioRepo.uploadPostPhoto(createdPost.id, files)
@@ -125,12 +127,12 @@ export class PostService implements IPostService {
 
     async editPost(dto: EditPostDTO, id: JustId, userId: UserId) {
         const postId = zodPostId.parse(id)
-        const post = (await this.postRepo.findWithoutDetailByID(postId))
+        const post = await this.postRepo.findWithoutDetailByID(postId)
         if (!post || post.author !== userId) return { msg: messages.postNotFound.persian }
         post.caption = dto.caption
         post.closeFriend = dto.closeFriend
         post.tags = dto.tags ?? []
-        const editedPost = (await this.postRepo.edit(post))
+        const editedPost = await this.postRepo.edit(post)
         if (!editedPost) return new ServerError(PersianErrors.ServerError)
 
         const result = await this.adjustPhoto(editedPost)
@@ -149,7 +151,7 @@ export class PostService implements IPostService {
 
     async getPost(id: JustId, userId: UserId) {
         const postId = zodPostId.parse(id)
-        const post = (await this.postRepo.findWithDetailByID(postId))
+        const post = await this.postRepo.findWithDetailByID(postId)
         if (!post) return { msg: messages.postNotFound.persian }
         const closeFriend = await this.checkCloseFriend(userId, post.author)
         if (closeFriend[0] === false && post.closeFriend === true) return { msg: messages.postAccessDenied.persian }
@@ -157,14 +159,14 @@ export class PostService implements IPostService {
         const bookmark = await (services['BookmarkService'] as BookmarkService).getBookmarkByUserAndPost(userId, post.id)
         post.ifBookmarked = bookmark
         post.ifLiked = like
-        
+
         const result = await this.adjustPhoto(post)
         return this.adjustPhoto(result)
     }
 
     async getPostWitoutDetail(id: JustId) {
         const postId = zodPostId.parse(id)
-        const post = (await this.postRepo.findWithoutDetailByID(postId))
+        const post = await this.postRepo.findWithoutDetailByID(postId)
         if (!post) return { msg: messages.postNotFound.persian }
 
         return post
@@ -188,6 +190,8 @@ export class PostService implements IPostService {
                 const photos = await MinioRepo.getPostPhotoUrl(post.id)
                 post.photos = photos
             }
+            const profilePhoto = await MinioRepo.getProfileUrl(user.id)
+            user.photo = profilePhoto || ''
             result.push({ user, posts })
         }
         return result
